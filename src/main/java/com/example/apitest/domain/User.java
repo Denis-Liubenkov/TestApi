4160cbe3ffbaae0dbe6321d993c9ab588fb9f643
Alligator89 <denisliubenkov@yandex.by>
2024-08-08 17:23:55 +0300

    добавление аннотаций в классе User

--- src/main/java/com/example/apitest/domain/User.java
+++ src/main/java/com/example/apitest/domain/User.java
@@ -1,18 +1,37 @@
 package com.example.apitest.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
+@Data
+@Entity(name = "users")
 public class User {
-private Integer id;
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private Integer id;
 
-private String name;
+    @Pattern(regexp = "^[А-Яа-яЁё]+$")
+    @Column(name = "name", nullable = false)
+    private String name;
 
-private String surName;
+    @Pattern(regexp = "^[А-Яа-яЁё]+$")
+    @Column(name = "sur_name", nullable = false)
+    private String surName;
 
-private String email;
+    @Email
+    @Column(name = "email", nullable = false)
+    private String email;
 
-private List<String>roles;
+    @ElementCollection
+    private List<String> roles;
 
-private List<String>phones;
-
+    @ElementCollection
+    private List<String> phones;
 }
