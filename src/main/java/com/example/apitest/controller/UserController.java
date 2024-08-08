bc60f9fbfffefd0165e394fa4af4c5ef90ba4b12
Alligator89 <denisliubenkov@yandex.by>
2024-08-08 17:41:24 +0300

    добавление аннотаций

--- src/main/java/com/example/apitest/controller/UserController.java
+++ src/main/java/com/example/apitest/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.example.apitest.controller;
 
 import com.example.apitest.domain.User;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
     private final ClientService clientService;
 
@@ -15,6 +17,7 @@ public class UserController {
         this.clientService = clientService;
     }
 
+    @GetMapping
     public ResponseEntity<List<User>> getInfoUsers() {
         List<User> users = clientService.getUsers();
         if (users.isEmpty()) {
@@ -24,22 +27,26 @@ public class UserController {
         }
     }
 
-    public ResponseEntity<User> getInfoUser(@PathVariable Integer id){
-User user = clientService.getUser(id).orElseThrow(UserNotFoundException::new);
-return new ResponseEntity<>(user,HttpStatus.OK);
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getInfoUser(@PathVariable Integer id) {
+        User user = clientService.getUser(id).orElseThrow(UserNotFoundException::new);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    public ResponseEntity<HttpStatus> createUser(@RequestBody User user){
-clientService.createUser(user);
-return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping("/create")
+    public ResponseEntity<HttpStatus> createUser(@Valid @RequestBody User user) {
+        clientService.createUser(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    public ResponseEntity<HttpStatus> updateUser(@RequestBody User user){
+    @PutMapping("/update")
+    public ResponseEntity<HttpStatus> updateUser(@Valid @RequestBody User user) {
         clientService.updateUser(user);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Integer id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Integer id) {
         clientService.deleteUserById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
