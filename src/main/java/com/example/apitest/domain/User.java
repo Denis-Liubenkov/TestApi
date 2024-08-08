54932928ffadc41acf79976732e35a4a63e77ef0
Alligator89 <denisliubenkov@yandex.by>
2024-08-08 17:15:48 +0300

    создание пакета domain и класса User

---
+++ src/main/java/com/example/apitest/domain/User.java
@@ -0,0 +1,18 @@
+package com.example.apitest.domain;
+
+import java.util.List;
+
+public class User {
+private Integer id;
+
+private String name;
+
+private String surName;
+
+private String email;
+
+private List<String>roles;
+
+private List<String>phones;
+
+}
